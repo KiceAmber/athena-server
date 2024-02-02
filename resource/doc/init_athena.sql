45ebc484de8ef1f15b905380cc33394c75e440fb
KiceAmber <kiceamber@gmail.com>
2024-02-02 21:38:35 +0800

    feat: 添加sql建表文件

--- resource/doc/init_athena.sql
+++ resource/doc/init_athena.sql
@@ -7,7 +7,7 @@ create table `tag` (
     `id` int not null auto_increment comment '标签ID',
     `name` varchar(64) not null comment '标签名称',
     `created_at` timestamp not null default current_timestamp comment '创建时间',
-    `updated_at` timestamp not null default current_timestamp on update current_timestamp comment '更新时间',
+    `updated_at` timestamp not null default current_timestamp on update current_timestamp comment '修改时间',
     `deleted_at` timestamp default null comment '删除时间',
     primary key (`id`)
 );
@@ -19,7 +19,35 @@ create table `article` (
     `content` text not null comment '文章内容',
     `tag_id` int not null comment '文章所属标签ID',
     `created_at` timestamp not null default current_timestamp comment '创建时间',
-    `updated_at` timestamp not null default current_timestamp on update current_timestamp comment '更新时间',
+    `updated_at` timestamp not null default current_timestamp on update current_timestamp comment '修改时间',
     `deleted_at` timestamp default null comment '删除时间',
     primary key (`id`)
+);
+
+-- table of category
+create table `category` (
+    `id` int not null auto_increment comment '分类ID',
+    `name` varchar(64) not null comment '分类名称',
+    `created_at` timestamp not null default current_timestamp comment '创建时间',
+    `updated_at` timestamp not null default current_timestamp on update current_timestamp comment '修改时间',
+    `deleted_at` timestamp default null comment '删除时间',
+)
+
+-- table of role
+create table `role` (
+    `id` int not null auto_increment comment '角色ID',
+    `name` varchar(64) not null comment '角色名称',
+    `created_at` timestamp not null default current_timestamp comment '创建时间',
+    `updated_at` timestamp not null default current_timestamp on update current_timestamp comment '修改时间',
+    `deleted_at` timestamp default null comment '删除时间',
+);
+
+-- table of menu
+create table `menu` (
+    `id` int not null auto_increment comment '菜单ID',
+    `name` varchar(64) not null comment '菜单名称',
+    `url` varchar(128) not null comment '菜单URL',
+    `created_at` timestamp not null default current_timestamp comment '创建时间',
+    `updated_at` timestamp not null default current_timestamp on update current_timestamp comment '修改时间',
+    `deleted_at` timestamp default null comment '删除时间',
 );
